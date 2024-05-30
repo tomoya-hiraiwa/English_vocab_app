5878df8f828a2d0d2fd5e9518ef150316a469143
tomoya-hiraiwa <tomoya5568.n.tfamily7667@gmail.com>
2024-05-30 14:20:15 +0900

    add some fragment

--- English_word_app/app/src/main/java/com/example/english_word_app/CreateSecondFragment.kt
+++ English_word_app/app/src/main/java/com/example/english_word_app/CreateSecondFragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.english_word_app.databinding.FragmentCreateSecondBinding
 
 
 class CreateSecondFragment : Fragment() {
-
+private lateinit var b: FragmentCreateSecondBinding
+private lateinit var v: CreateQuizViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_second, container, false)
+        b = FragmentCreateSecondBinding.inflate(inflater)
+        return b.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        v = ViewModelProvider(requireActivity())[CreateQuizViewModel::class.java]
+        b.apply {
+            nextButton.setOnClickListener {
+                v.stepCount.value = 2
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.addQuizFragmentContainer,CreateThirdFragment())
+                    .commit()
+            }
+        }
     }
 
    
